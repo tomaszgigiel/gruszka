5f58c65f9c7f408d74f7d7ebc9e599ae30cd6590
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-08-03 21:36:20 +0200

    read barcode

--- src/main/clojure/pl/tomaszgigiel/gruszka/core.clj
+++ src/main/clojure/pl/tomaszgigiel/gruszka/core.clj
@@ -1,7 +1,14 @@
 (ns pl.tomaszgigiel.gruszka.core
+  (:import java.awt.image.BufferedImage)
+  (:import java.io.File)
   (:import java.nio.file.Paths)
+  (:import javax.imageio.ImageIO)
   (:import com.google.zxing.BarcodeFormat)
+  (:import com.google.zxing.BinaryBitmap)
+  (:import com.google.zxing.MultiFormatReader)
+  (:import com.google.zxing.client.j2se.BufferedImageLuminanceSource)
   (:import com.google.zxing.client.j2se.MatrixToImageWriter)
+  (:import com.google.zxing.common.HybridBinarizer)
   (:import com.google.zxing.qrcode.QRCodeWriter)
   (:gen-class))
 
@@ -10,8 +17,13 @@
         o (Paths/get p (into-array [""]))]
     (MatrixToImageWriter/writeToPath q x o)))
 
+(defn barcode-string [p] 
+  (.getText (.decode (new MultiFormatReader)(new BinaryBitmap (new HybridBinarizer (new BufferedImageLuminanceSource (ImageIO/read (new File p))))))))
+
 (defn -main
   "gruszka: pictures against younger sister"
   [& args]
-  (create-barcode (nth args 0) (Integer/parseInt (nth args 1)) (Integer/parseInt (nth args 2)) (nth args 3) (nth args 4))
+  (cond
+    (= (count args) 5)(create-barcode (nth args 0) (Integer/parseInt (nth args 1)) (Integer/parseInt (nth args 2)) (nth args 3) (nth args 4))
+    (= (count args) 1)(println (barcode-string (first args))))
   (println "ok"))
